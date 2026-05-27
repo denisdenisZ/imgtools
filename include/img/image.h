bfd1cb4f875469255903e4d33f5ece86d50653da
Denis Zlatanov <bombata053@protonmail.com>
2026-05-27 07:29:57 +0300

    feat: Add image format normalization

--- include/img/image.h
+++ include/img/image.h
@@ -12,7 +12,9 @@ typedef enum {
     BGGR,
     GRBG,
     GBGR,
+    GBRG,
     RGBIR,
+    BAY_PAT_MAX
 } e_bayer_pat;
 
 typedef enum {
@@ -56,11 +58,31 @@ static const e_endianness ENDIANESS_DEF = ENDIAN_LITTLE;
 static const e_packing PACKING_DEF      = UNPACKED;
 
 typedef struct {
+    int8_t dx;
+    int8_t dy;
+} st_bayer_offset;
+
+typedef enum {
+    BAYER_R  = 0,
+    BAYER_GR = 1,
+    BAYER_GB = 2,
+    BAYER_B  = 3
+} e_bayer_channel;
+
+static const st_bayer_offset bayer_to_rggb[BAY_PAT_MAX][4] = {
+//                R        Gr       Gb       B
+    [RGGB] = {{0,0},   {1,0},   {0,1},   {1,1}},  // reference
+    [GRBG] = {{1,0},   {0,0},   {1,1},   {0,1}},  // R is one pixel right
+    [GBRG] = {{0,1},   {1,1},   {0,0},   {1,0}},  // R is one line down
+    [BGGR] = {{1,1},   {0,1},   {1,0},   {0,0}},  // R is one pixel right and one line down
+};
+
+typedef struct {
     bpp_t bpp;
     e_bayer_pat pattern;
     e_packing packing;
     e_bit_align bit_align;
-    e_endianness endianess;
+    e_endianness endianness;
 
     st_compand_info *compand;
 
@@ -71,15 +93,36 @@ typedef struct {
     uint32_t remb_lines_cnt;
     uint32_t black_level;
 
+    // NOTE: Does not count femb, 0,0 is the first valid bayer pixel
     uint32_t offset_x;
     uint32_t offset_y;
 
     uint8_t *data;
+    uint32_t data_size;
 } st_raw_bayer_img;
+
+typedef struct {
+    st_compand_info *compand;
+    e_bayer_pat pattern;
+
+    uint32_t width;
+    uint32_t height;
+    uint32_t femb_lines_cnt;
+    uint32_t remb_lines_cnt;
+    uint32_t black_level;
+
+    // NOTE: Does not count femb, 0,0 is the first valid bayer pixel
+    uint32_t offset_x;
+    uint32_t offset_y;
+
+    uint32_t *data;
+    uint32_t data_size;
+} st_norm_bayer_img;
 
 typedef enum {
     IMG_SUCCESS,
     IMG_INVALID_CFG,
+    IMG_FAIL_LOAD,
 } e_img_status;
 
 typedef struct {
@@ -90,6 +133,7 @@ typedef struct {
     e_packing packing;
     e_bit_align bit_align;
     e_endianness endianness;
+
     st_compand_info *compand;
 
     uint32_t width;
@@ -103,7 +147,22 @@ typedef struct {
     uint32_t offset_x;
     uint32_t offset_y;
 } st_raw_bayer_img_cfg;
-e_img_status create_img(st_raw_bayer_img_cfg *cfg, st_raw_bayer_img *out);
+
+static inline uint32_t get_bayer_channel(
+    const uint32_t *data,
+    uint32_t stride,
+    e_bayer_pat pattern,
+    uint32_t x,
+    uint32_t y,
+    e_bayer_channel channel)
+{
+    uint32_t px = x + bayer_to_rggb[pattern][channel].dx;
+    uint32_t py = y + bayer_to_rggb[pattern][channel].dy;
+    return data[py * stride + px];
+}
+
+e_img_status create_img(const st_raw_bayer_img_cfg *cfg, st_raw_bayer_img *out);
+e_img_status normalize_img(const st_raw_bayer_img *img, st_norm_bayer_img *out);
 
 #endif
 
--- src/img/image.c
+++ src/img/image.c
@@ -1,9 +1,12 @@
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
 #include "image.h"
+#include "io/io.h"
 
 static inline bool is_whithin_bounds(uint32_t min, uint32_t max, uint32_t val, const char *what) {
     if (val < min) {
@@ -17,7 +20,7 @@ static inline bool is_whithin_bounds(uint32_t min, uint32_t max, uint32_t val, c
     return true;
 }
 
-static inline bool is_cfg_valid(st_raw_bayer_img_cfg *cfg) {
+static inline bool is_cfg_valid(const st_raw_bayer_img_cfg *cfg) {
     if (cfg == NULL) {
         fprintf(stderr, "Invalid config, is NULL\n");
         return false;
@@ -31,12 +34,106 @@ static inline bool is_cfg_valid(st_raw_bayer_img_cfg *cfg) {
     return valid;
 }
 
-e_img_status create_img(st_raw_bayer_img_cfg *cfg, st_raw_bayer_img *out) {
+e_img_status create_img(const st_raw_bayer_img_cfg *cfg, st_raw_bayer_img *out) {
     if (is_cfg_valid(cfg) == false) {
         return IMG_INVALID_CFG;
     }
-// TODO:
 
+    memset(out, 0, sizeof(st_raw_bayer_img));
+
+    out->bpp = cfg->bpp;
+    out->pattern = cfg->pattern;
+    out->packing = cfg->packing;
+    out->bit_align = cfg->bit_align;
+    out->endianness = cfg->endianness;
+    out->compand = cfg->compand;
+    out->width = cfg->width;
+    out->height = cfg->height;
+    out->femb_lines_cnt = cfg->femb_lines_cnt;
+    out->remb_lines_cnt = cfg->remb_lines_cnt;
+    out->black_level = cfg->black_level;
+    out->offset_x = cfg->offset_x;
+    out->offset_y = cfg->offset_y;
+    // TODO: This won't work for packed images
+    out->stride = cfg->width * ((cfg->bpp + 7) / 8);
+    out->data = calloc(out->stride * (out->height + out->femb_lines_cnt + out->remb_lines_cnt), 1);
+
+    out->data_size = 0;
+    read_file(cfg->filename, &out->data, &out->data_size);
+    if (!out->data_size) {
+        return IMG_FAIL_LOAD;
+    }
+
+    return IMG_SUCCESS;
+}
+
+e_img_status destroy_raw_img(st_raw_bayer_img *img) {
+    if (!img) return IMG_INVALID_CFG;
+    free(img->data);
+    img->data = NULL;
+    img->data_size = 0;
+    return IMG_SUCCESS;
+}
+
+static inline void norm_bpp(uint8_t *data, uint32_t size, bpp_t bpp, uint32_t *out) {
+    switch(bpp) {
+        case 8:
+            for (uint32_t i = 0; i < size; i++) {
+                out[i] = ((uint8_t *)data)[i];
+            }
+            break;
+        case 10:
+        case 12:
+        case 14:
+        case 16:
+            for (uint32_t i = 0; i < size; i++) {
+                out[i] = ((uint16_t *)data)[i];
+            }
+            break;
+        case 32:
+            for (uint32_t i = 0; i < size; i++) {
+                out[i] = ((uint32_t *)data)[i];
+            }
+            break;
+    }
+}
+
+static inline void norm_endianness(uint32_t *data, uint32_t size, e_endianness endianness) {
+    if (endianness == ENDIAN_BIG) {
+        for (uint32_t i = 0; i < size; i++) {
+            data[i] = __builtin_bswap32(data[i]);
+        }
+    }
+}
+
+static inline void norm_bit_align(uint32_t *data, uint32_t size, bpp_t bpp, e_bit_align bit_align) {
+    if (bit_align == BIT_ALIGN_MSB) {
+        // NOTE: 32 assumins we have already normalized bpp
+        const uint32_t shift = 32 - bpp;
+        for (uint32_t i = 0; i < size; i++) {
+            data[i] >>= shift;
+        }
+    }
+}
+
+e_img_status normalize_img(const st_raw_bayer_img *img, st_norm_bayer_img *out) {
+    memset(out, 0, sizeof(st_norm_bayer_img));
+
+    out->pattern = img->pattern;
+    out->compand = img->compand;
+    out->width = img->width;
+    out->height = img->height;
+    out->femb_lines_cnt = img->femb_lines_cnt;
+    out->remb_lines_cnt = img->remb_lines_cnt;
+    out->black_level = img->black_level;
+    out->offset_x = img->offset_x;
+    out->offset_y = img->offset_y;
+    out->data_size = out->width * (out->femb_lines_cnt + out->remb_lines_cnt + out->height);
+    out->data = calloc(out->data_size, sizeof(uint32_t));
+
+    norm_bpp(img->data, out->data_size, img->bpp, out->data);
+    norm_endianness(out->data, out->data_size, img->endianness);
+    norm_bit_align(out->data, out->data_size, img->bpp, img->bit_align);
 
     return IMG_SUCCESS;
 }
